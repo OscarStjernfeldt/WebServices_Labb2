320be17b231150b41e52e005c0893e9fe98fafc1
OscarStjernfeldt <oscarstjernfeldt@hotmail.com>
2022-04-26 16:06:55 +0200

    Add entity: Fruit

---
+++ src/main/java/com/example/entity/Fruit.java
@@ -0,0 +1,16 @@
+package com.example.entity;
+
+public class Fruit {
+
+    private final String fruit;
+
+    public Fruit(String fruit) {
+        this.fruit = fruit;
+    }
+
+    public String getFruit() {
+        return fruit;
+    }
+
+
+}
