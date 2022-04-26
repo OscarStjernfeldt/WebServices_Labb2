721a8c2e63fa99d8277186170a80d94c37de5527
OscarStjernfeldt <oscar.eriksson.stjernfeldt@iths.se>
2022-04-26 14:13:37 +0200

    Add entity Soda

---
+++ src/main/java/com/example/entity/Soda.java
@@ -0,0 +1,14 @@
+package com.example.entity;
+
+public class Soda {
+
+    private final String soda;
+
+    public Soda(String soda) {
+        this.soda = soda;
+    }
+
+    public String getSoda() {
+        return soda;
+    }
+}
