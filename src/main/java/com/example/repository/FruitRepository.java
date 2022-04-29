59e9617e204e62022f64f21f7f3f3680339c8591
OscarStjernfeldt <oscarstjernfeldt@hotmail.com>
2022-04-29 14:16:59 +0200

    Refactor code

--- src/main/java/com/example/repository/FruitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.Fruit;
-import io.micronaut.data.repository.CrudRepository;
-
-public interface FruitRepository extends CrudRepository<Fruit, String> {
-
-
-}
