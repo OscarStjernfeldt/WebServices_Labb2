4e0ea411c82029db173a00b89ed4b41af7ba9a45
OscarStjernfeldt <oscarstjernfeldt@hotmail.com>
2022-04-29 14:13:16 +0200

    Update class: Soda.java

--- src/main/java/com/example/entity/Soda.java
+++ src/main/java/com/example/entity/Soda.java
@@ -1,14 +1,47 @@
 package com.example.entity;
 
+import io.micronaut.core.annotation.Creator;
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.core.annotation.ReflectiveAccess;
+import org.bson.codecs.pojo.annotations.BsonCreator;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+
+import javax.validation.constraints.NotBlank;
+
+@Introspected
+@ReflectiveAccess
 public class Soda {
 
-    private final String soda;
+    @NonNull
+    @NotBlank
+    @BsonProperty("name")
+    private final String name;
 
-    public Soda(String soda) {
-        this.soda = soda;
+    @Nullable
+    @BsonProperty("description")
+    private final String description;
+
+    public Soda(@NonNull String name) {
+        this(name, null);
     }
 
-    public String getSoda() {
-        return soda;
+    @Creator
+    @BsonCreator
+    public Soda(@NonNull @BsonProperty("name") String name,
+                @Nullable @BsonProperty("description") String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @NonNull
+    public String getName() {
+        return name;
+    }
+
+    @Nullable
+    public String getDescription() {
+        return description;
     }
 }
